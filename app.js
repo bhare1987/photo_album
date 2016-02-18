eaed0602bdf0e3feb7b092f3bbfba6df554b5b08
bhare1987 <brandonlhare@gmail.com>
2016-02-18 16:50:59 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function buildRows(array) {
+  var row = "<div class='row'";
+  array.forEach(function(el, idx){
+    row += "<div class='rowItem'>"
+        + "<img src='"
+        + el.
+    if (idx % 3 === 0) {
+
+    }
+  })
+}
---
+++ imgs.js
@@ -0,0 +1,186 @@
+var imgs = [{
+  album: "album1",
+  album_url: "img/album1/",
+  album_cover: "photo1_1",
+  photos: [
+    {
+      photo_name: "photo1_1",
+      photo_path: "photo1_1.jpeg"
+    },
+    {
+      photo_name: "photo1_2",
+      photo_path: "photo1_2.jpeg"
+    },
+    {
+      photo_name: "photo1_3",
+      photo_path: "photo1_3.jpeg"
+    },
+    {
+      photo_name: "photo1_4",
+      photo_path: "photo1_4.jpeg"
+    },
+    {
+      photo_name: "photo1_5",
+      photo_path: "photo1_5.jpeg"
+    },
+    {
+      photo_name: "photo1_6",
+      photo_path: "photo1_6.jpeg"
+    }
+  ]
+},
+{
+  album: "album2",
+  album_url: "img/album2/",
+  album_cover: "photo2_1",
+  photos: [
+    {
+      photo_name: "photo2_1",
+      photo_path: "photo2_1.jpeg"
+    },
+    {
+      photo_name: "photo2_2",
+      photo_path: "photo2_2.jpeg"
+    },
+    {
+      photo_name: "photo2_3",
+      photo_path: "photo2_3.jpeg"
+    },
+    {
+      photo_name: "photo2_4",
+      photo_path: "photo2_4.jpeg"
+    },
+    {
+      photo_name: "photo2_5",
+      photo_path: "photo2_5.jpeg"
+    },
+    {
+      photo_name: "photo2_6",
+      photo_path: "photo2_6.jpeg"
+    }
+  ]
+},
+{
+  album: "album3",
+  album_url: "img/album3/",
+  album_cover: "photo3_1",
+  photos: [
+    {
+      photo_name: "photo3_1",
+      photo_path: "photo3_1.jpeg"
+    },
+    {
+      photo_name: "photo3_2",
+      photo_path: "photo3_2.jpeg"
+    },
+    {
+      photo_name: "photo3_3",
+      photo_path: "photo3_3.jpeg"
+    },
+    {
+      photo_name: "photo3_4",
+      photo_path: "photo3_4.jpeg"
+    },
+    {
+      photo_name: "photo3_5",
+      photo_path: "photo3_5.jpeg"
+    },
+    {
+      photo_name: "photo3_6",
+      photo_path: "photo3_6.jpeg"
+    }
+  ]
+},
+{
+  album: "album4",
+  album_url: "img/album4/",
+  album_cover: "photo4_1",
+  photos: [
+    {
+      photo_name: "photo4_1",
+      photo_path: "photo4_1.jpeg"
+    },
+    {
+      photo_name: "photo4_2",
+      photo_path: "photo4_2.jpeg"
+    },
+    {
+      photo_name: "photo4_3",
+      photo_path: "photo4_3.jpeg"
+    },
+    {
+      photo_name: "photo4_4",
+      photo_path: "photo4_4.jpeg"
+    },
+    {
+      photo_name: "photo4_5",
+      photo_path: "photo4_5.jpeg"
+    },
+    {
+      photo_name: "photo4_6",
+      photo_path: "photo4_6.jpeg"
+    }
+  ]
+},
+{
+  album: "album5",
+  album_url: "img/album5/",
+  album_cover: "photo5_1",
+  photos: [
+    {
+      photo_name: "photo5_1",
+      photo_path: "photo5_1.jpeg"
+    },
+    {
+      photo_name: "photo5_2",
+      photo_path: "photo5_2.jpeg"
+    },
+    {
+      photo_name: "photo5_3",
+      photo_path: "photo5_3.jpeg"
+    },
+    {
+      photo_name: "photo5_4",
+      photo_path: "photo5_4.jpeg"
+    },
+    {
+      photo_name: "photo5_5",
+      photo_path: "photo5_5.jpeg"
+    },
+    {
+      photo_name: "photo5_6",
+      photo_path: "photo5_6.jpeg"
+    }
+  ]
+},
+{
+  album: "album6",
+  album_url: "img/album6/",
+  album_cover: "photo6_1",
+  photos: [
+    {
+      photo_name: "photo6_1",
+      photo_path: "photo6_1.jpeg"
+    },
+    {
+      photo_name: "photo6_2",
+      photo_path: "photo6_2.jpeg"
+    },
+    {
+      photo_name: "photo6_3",
+      photo_path: "photo6_3.jpeg"
+    },
+    {
+      photo_name: "photo6_4",
+      photo_path: "photo6_4.jpeg"
+    },
+    {
+      photo_name: "photo6_5",
+      photo_path: "photo6_5.jpeg"
+    },
+    {
+      photo_name: "photo6_6",
+      photo_path: "photo6_6.jpeg"
+    }
+  ]
+}];
