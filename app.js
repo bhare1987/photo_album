c09d0ab5b7ecfcaf32e1f326ab801e738be60502
bhare1987 <brandonlhare@gmail.com>
2016-02-19 10:55:41 -0500

    added sidebar nav

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
-  $(".albums").html(buildAlbumRows(albumMap(imgs)));
-  $(".images").html(buildImageRows(imageMap(imgs)));
+  $(".albums").append(buildAlbumRows(albumMap(imgs)));
+  $("i").addClass("fa-lg");
+  $(".sideNav").append(buildSideNav(albumMap(imgs)));
+  $(".rowItem[data-type='album']").hover(function(){
+    $(this).children("i").addClass("fa-folder-open-o").removeClass("fa-folder-o");
+  }, function(){
+    $(this).children("i").addClass("fa-folder-o").removeClass("fa-folder-open-o");
+  });
+  $(".rowItem[data-type='album']").on("click", function() {
+    var images = imageMap(imgs);
+    var albumName = $(this).children("img").attr("rel");
+    images = images.filter(function(el){
+      return el.album === albumName;
+    });
+    $(".images").append(buildImageRows(images));
+    $(".imagesContainer").addClass("active");
+    $(".albums").removeClass("active");
+  });
 
 });
 
 function buildAlbumRows(array) {
   var row = "<div class='row'>";
   array.forEach(function(el, idx, arr){
-    row += "<div class='rowItem'>"
+    row += "<div class='rowItem' data-type='"
+        + el.type + "'>"
+        + "<i class='fa " + el.icon + "'></i>"
         + "<img src='"
         + el.url + "thumbnails/" + el.cover + ".png"
         + "' rel='" + el.name + "'>"
@@ -28,7 +46,8 @@ function buildAlbumRows(array) {
 function buildImageRows(array) {
   var row = "<div class='row'>";
   array.forEach(function(el, idx, arr){
-    row += "<div class='rowItem'>"
+    row += "<div class='rowItem' data-type='"
+        + el.type + "'>"
         + "<img src='"
         + el.photo_path_thumb
         + "' rel='" + el.photo_name + "'>"
@@ -51,7 +70,9 @@ function albumMap(array){
     return {
       name: el.album,
       url: el.album_url,
-      cover: el.album_cover
+      cover: el.album_cover,
+      icon: el.album_icon,
+      type: el.type
     }
   });
   return result;
@@ -60,7 +81,16 @@ function albumMap(array){
 function imageMap(array){
   var result = [];
   array.forEach(function(el){
-  result.push( _.flatten(el.photos, true));
+  result.push(el.photos);
   })
   return _.flatten(result, true);
+}
+
+function buildSideNav(array) {
+  var sideNav = "<ul>";
+  array.forEach(function(el){
+    sideNav += "<li>" + el.name + "</li>"
+  });
+  sideNav += "</ul>";
+  return sideNav;
 }
--- imgs.js
+++ imgs.js
@@ -2,36 +2,50 @@ var imgs = [{
   album: "album1",
   album_url: "img/album1/",
   album_cover: "photo1_1",
+  album_icon: "fa-folder-o",
+  type: "album",
   photos: [
     {
       photo_name: "photo1_1",
       photo_path_hr: "img/album1/high_res/photo1_1.jpeg",
-      photo_path_thumb: "img/album1/thumbnails/photo1_1.png"
+      photo_path_thumb: "img/album1/thumbnails/photo1_1.png",
+      type: "image",
+      album: "album1"
     },
     {
       photo_name: "photo1_2",
       photo_path_hr: "img/album1/high_res/photo1_2.jpeg",
-      photo_path_thumb: "img/album1/thumbnails/photo1_2.png"
+      photo_path_thumb: "img/album1/thumbnails/photo1_2.png",
+      type: "image",
+      album: "album1"
     },
     {
       photo_name: "photo1_3",
       photo_path_hr: "img/album1/high_res/photo1_3.jpeg",
-      photo_path_thumb: "img/album1/thumbnails/photo1_3.png"
+      photo_path_thumb: "img/album1/thumbnails/photo1_3.png",
+      type: "image",
+      album: "album1"
     },
     {
       photo_name: "photo1_4",
       photo_path_hr: "img/album1/high_res/photo1_4.jpeg",
-      photo_path_thumb: "img/album1/thumbnails/photo1_4.png"
+      photo_path_thumb: "img/album1/thumbnails/photo1_4.png",
+      type: "image",
+      album: "album1"
     },
     {
       photo_name: "photo1_5",
       photo_path_hr: "img/album1/high_res/photo1_5.jpeg",
-      photo_path_thumb: "img/album1/thumbnails/photo1_5.png"
+      photo_path_thumb: "img/album1/thumbnails/photo1_5.png",
+      type: "image",
+      album: "album1"
     },
     {
       photo_name: "photo1_6",
       photo_path_hr: "img/album1/high_res/photo1_6.jpeg",
-      photo_path_thumb: "img/album1/thumbnails/photo1_6.png"
+      photo_path_thumb: "img/album1/thumbnails/photo1_6.png",
+      type: "image",
+      album: "album1"
     }
   ]
 },
@@ -39,36 +53,50 @@ var imgs = [{
   album: "album2",
   album_url: "img/album2/",
   album_cover: "photo2_1",
+  album_icon: "fa-folder-o",
+  type: "album",
   photos: [
     {
       photo_name: "photo2_1",
       photo_path_hr: "img/album2/high_res/photo2_1.jpeg",
-      photo_path_thumb: "img/album2/thumbnails/photo2_1.png"
+      photo_path_thumb: "img/album2/thumbnails/photo2_1.png",
+      type: "image",
+      album: "album2"
     },
     {
       photo_name: "photo2_2",
       photo_path_hr: "img/album2/high_res/photo2_2.jpeg",
-      photo_path_thumb: "img/album2/thumbnails/photo2_2.png"
+      photo_path_thumb: "img/album2/thumbnails/photo2_2.png",
+      type: "image",
+      album: "album2"
     },
     {
       photo_name: "photo2_3",
       photo_path_hr: "img/album2/high_res/photo2_3.jpeg",
-      photo_path_thumb: "img/album2/thumbnails/photo2_3.png"
+      photo_path_thumb: "img/album2/thumbnails/photo2_3.png",
+      type: "image",
+      album: "album2"
     },
     {
       photo_name: "photo2_4",
       photo_path_hr: "img/album2/high_res/photo2_4.jpeg",
-      photo_path_thumb: "img/album2/thumbnails/photo2_4.png"
+      photo_path_thumb: "img/album2/thumbnails/photo2_4.png",
+      type: "image",
+      album: "album2"
     },
     {
       photo_name: "photo2_5",
       photo_path_hr: "img/album2/high_res/photo2_5.jpeg",
-      photo_path_thumb: "img/album2/thumbnails/photo2_5.png"
+      photo_path_thumb: "img/album2/thumbnails/photo2_5.png",
+      type: "image",
+      album: "album2"
     },
     {
       photo_name: "photo2_6",
       photo_path_hr: "img/album2/high_res/photo2_6.jpeg",
-      photo_path_thumb: "img/album2/thumbnails/photo2_6.png"
+      photo_path_thumb: "img/album2/thumbnails/photo2_6.png",
+      type: "image",
+      album: "album2"
     }
   ]
 },
@@ -76,36 +104,50 @@ var imgs = [{
   album: "album3",
   album_url: "img/album3/",
   album_cover: "photo3_1",
+  album_icon: "fa-folder-o",
+  type: "album",
   photos: [
     {
       photo_name: "photo3_1",
       photo_path_hr: "img/album3/high_res/photo3_1.jpeg",
-      photo_path_thumb: "img/album3/thumbnails/photo3_1.png"
+      photo_path_thumb: "img/album3/thumbnails/photo3_1.png",
+      type: "image",
+      album: "album3"
     },
     {
       photo_name: "photo3_2",
       photo_path_hr: "img/album3/high_res/photo3_2.jpeg",
-      photo_path_thumb: "img/album3/thumbnails/photo3_2.png"
+      photo_path_thumb: "img/album3/thumbnails/photo3_2.png",
+      type: "image",
+      album: "album3"
     },
     {
       photo_name: "photo3_3",
       photo_path_hr: "img/album3/high_res/photo3_3.jpeg",
-      photo_path_thumb: "img/album3/thumbnails/photo3_3.png"
+      photo_path_thumb: "img/album3/thumbnails/photo3_3.png",
+      type: "image",
+      album: "album3"
     },
     {
       photo_name: "photo3_4",
       photo_path_hr: "img/album3/high_res/photo3_4.jpeg",
-      photo_path_thumb: "img/album3/thumbnails/photo3_4.png"
+      photo_path_thumb: "img/album3/thumbnails/photo3_4.png",
+      type: "image",
+      album: "album3"
     },
     {
       photo_name: "photo3_5",
       photo_path_hr: "img/album3/high_res/photo3_5.jpeg",
-      photo_path_thumb: "img/album3/thumbnails/photo3_5.png"
+      photo_path_thumb: "img/album3/thumbnails/photo3_5.png",
+      type: "image",
+      album: "album3"
     },
     {
       photo_name: "photo3_6",
       photo_path_hr: "img/album3/high_res/photo3_6.jpeg",
-      photo_path_thumb: "img/album3/thumbnails/photo3_6.png"
+      photo_path_thumb: "img/album3/thumbnails/photo3_6.png",
+      type: "image",
+      album: "album3"
     }
   ]
 },
@@ -113,36 +155,50 @@ var imgs = [{
   album: "album4",
   album_url: "img/album4/",
   album_cover: "photo4_1",
+  album_icon: "fa-folder-o",
+  type: "album",
   photos: [
     {
       photo_name: "photo4_1",
       photo_path_hr: "img/album4/high_res/photo4_1.jpeg",
-      photo_path_thumb: "img/album4/thumbnails/photo4_1.png"
+      photo_path_thumb: "img/album4/thumbnails/photo4_1.png",
+      type: "image",
+      album: "album4"
     },
     {
       photo_name: "photo4_2",
       photo_path_hr: "img/album4/high_res/photo4_2.jpeg",
-      photo_path_thumb: "img/album4/thumbnails/photo4_2.png"
+      photo_path_thumb: "img/album4/thumbnails/photo4_2.png",
+      type: "image",
+      album: "album4"
     },
     {
       photo_name: "photo4_3",
       photo_path_hr: "img/album4/high_res/photo4_3.jpeg",
-      photo_path_thumb: "img/album4/thumbnails/photo4_3.png"
+      photo_path_thumb: "img/album4/thumbnails/photo4_3.png",
+      type: "image",
+      album: "album4"
     },
     {
       photo_name: "photo4_4",
       photo_path_hr: "img/album4/high_res/photo4_4.jpeg",
-      photo_path_thumb: "img/album4/thumbnails/photo4_4.png"
+      photo_path_thumb: "img/album4/thumbnails/photo4_4.png",
+      type: "image",
+      album: "album4"
     },
     {
       photo_name: "photo4_5",
       photo_path_hr: "img/album4/high_res/photo4_5.jpeg",
-      photo_path_thumb: "img/album4/thumbnails/photo4_5.png"
+      photo_path_thumb: "img/album4/thumbnails/photo4_5.png",
+      type: "image",
+      album: "album4"
     },
     {
       photo_name: "photo4_6",
       photo_path_hr: "img/album4/high_res/photo4_6.jpeg",
-      photo_path_thumb: "img/album4/thumbnails/photo4_6.png"
+      photo_path_thumb: "img/album4/thumbnails/photo4_6.png",
+      type: "image",
+      album: "album4"
     }
   ]
 },
@@ -150,36 +206,50 @@ var imgs = [{
   album: "album5",
   album_url: "img/album5/",
   album_cover: "photo5_1",
+  album_icon: "fa-folder-o",
+  type: "album",
   photos: [
     {
       photo_name: "photo5_1",
       photo_path_hr: "img/album5/high_res/photo5_1.jpeg",
-      photo_path_thumb: "img/album5/thumbnails/photo5_1.png"
+      photo_path_thumb: "img/album5/thumbnails/photo5_1.png",
+      type: "image",
+      album: "album5"
     },
     {
       photo_name: "photo5_2",
       photo_path_hr: "img/album5/high_res/photo5_2.jpeg",
-      photo_path_thumb: "img/album5/thumbnails/photo5_2.png"
+      photo_path_thumb: "img/album5/thumbnails/photo5_2.png",
+      type: "image",
+      album: "album5"
     },
     {
       photo_name: "photo5_3",
       photo_path_hr: "img/album5/high_res/photo5_3.jpeg",
-      photo_path_thumb: "img/album5/thumbnails/photo5_3.png"
+      photo_path_thumb: "img/album5/thumbnails/photo5_3.png",
+      type: "image",
+      album: "album5"
     },
     {
       photo_name: "photo5_4",
       photo_path_hr: "img/album5/high_res/photo5_4.jpeg",
-      photo_path_thumb: "img/album5/thumbnails/photo5_4.png"
+      photo_path_thumb: "img/album5/thumbnails/photo5_4.png",
+      type: "image",
+      album: "album5"
     },
     {
       photo_name: "photo5_5",
       photo_path_hr: "img/album5/high_res/photo5_5.jpeg",
-      photo_path_thumb: "img/album5/thumbnails/photo5_5.png"
+      photo_path_thumb: "img/album5/thumbnails/photo5_5.png",
+      type: "image",
+      album: "album5"
     },
     {
       photo_name: "photo5_6",
       photo_path_hr: "img/album5/high_res/photo5_6.jpeg",
-      photo_path_thumb: "img/album5/thumbnails/photo5_6.png"
+      photo_path_thumb: "img/album5/thumbnails/photo5_6.png",
+      type: "image",
+      album: "album5"
     }
   ]
 },
@@ -187,36 +257,50 @@ var imgs = [{
   album: "album6",
   album_url: "img/album6/",
   album_cover: "photo6_1",
+  album_icon: "fa-folder-o",
+  type: "album",
   photos: [
     {
       photo_name: "photo6_1",
       photo_path_hr: "img/album6/high_res/photo6_1.jpeg",
-      photo_path_thumb: "img/album6/thumbnails/photo6_1.png"
+      photo_path_thumb: "img/album6/thumbnails/photo6_1.png",
+      type: "image",
+      album: "album6"
     },
     {
       photo_name: "photo6_2",
       photo_path_hr: "img/album6/high_res/photo6_2.jpeg",
-      photo_path_thumb: "img/album6/thumbnails/photo6_2.png"
+      photo_path_thumb: "img/album6/thumbnails/photo6_2.png",
+      type: "image",
+      album: "album6"
     },
     {
       photo_name: "photo6_3",
       photo_path_hr: "img/album6/high_res/photo6_3.jpeg",
-      photo_path_thumb: "img/album6/thumbnails/photo6_3.png"
+      photo_path_thumb: "img/album6/thumbnails/photo6_3.png",
+      type: "image",
+      album: "album6"
     },
     {
       photo_name: "photo6_4",
       photo_path_hr: "img/album6/high_res/photo6_4.jpeg",
-      photo_path_thumb: "img/album6/thumbnails/photo6_4.png"
+      photo_path_thumb: "img/album6/thumbnails/photo6_4.png",
+      type: "image",
+      album: "album6"
     },
     {
       photo_name: "photo6_5",
       photo_path_hr: "img/album6/high_res/photo6_5.jpeg",
-      photo_path_thumb: "img/album6/thumbnails/photo6_5.png"
+      photo_path_thumb: "img/album6/thumbnails/photo6_5.png",
+      type: "image",
+      album: "album6"
     },
     {
       photo_name: "photo6_6",
       photo_path_hr: "img/album6/high_res/photo6_6.jpeg",
-      photo_path_thumb: "img/album6/thumbnails/photo6_6.png"
+      photo_path_thumb: "img/album6/thumbnails/photo6_6.png",
+      type: "image",
+      album: "album6"
     }
   ]
 }];
